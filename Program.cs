a0a2b92e0f4fdfc0c84f21bdcdc307329dde1625
Raghadahmed44 <121640917+Raghadahmed44@users.noreply.github.com>
2022-12-30 14:38:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    class Program
+    {
+        static bool IsPerfect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                    sum = sum + i;
+            }
+            if (sum == number) return true;
+            else return false;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("please enter the first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the second number:");
+            y = int.Parse(Console.ReadLine());
+            for (int i = x; i <= y; i++)
+            {
+                if (IsPerfect(i)) Console.WriteLine("{0}", i);
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}  
+
